c6e4a499a44a8b0152e08be3609bee7b6fb8fd79
Mathias Moen <mathiasbbmoen@gmail.com>
2025-12-09 13:56:21 +0100

    Structured project, code more readable

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,11 +2,9 @@ extends Node2D
 
 var main_speed := 10.0
 
-@onready var map := $Sprite2D
-@export var stepper := 50.0
-
 var brake = false
 
+@onready var screen_size = get_viewport().get_visible_rect().size
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,19 +14,23 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
+	if $road.position.y >= screen_size.y:
+		print("HALLO")
+	
 	print(main_speed)
 	
-	if brake == true:
-		map.position.y += stepper * delta * 0.2 * main_speed
-		
-		if main_speed >= 1:
+	
+	
+	
+	if brake == true:	
+		if main_speed >= 0:
 			main_speed -= 10 * delta
+		$road.position.y += main_speed * delta
 	else:
-		
+		$road.position.y += main_speed * delta
 		main_speed += 10 * delta
 		
-		
-		map.position.y += stepper * main_speed * delta
+
 	if Input.is_action_pressed("brake"):
 		brake = true
 	else:
