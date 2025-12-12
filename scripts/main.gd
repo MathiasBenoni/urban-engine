60384d3170e7123c98a67626725bcad39ccedaea
Mathias Moen <mathiasbbmoen@gmail.com>
2025-12-12 15:22:43 +0100

    Added a list that the road generates to

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,6 +5,31 @@ var max_speed := 30000.0
 var brake_force := 1000.0
 @onready var screen_size = get_viewport().get_visible_rect().size
 var road_list = []
+var roads_made := 0
+
+
+func generate_pattern(length) -> Array:
+	var pattern = []
+	var zeros_since_last_one = 20  # Start at 20 so first element can be a 1
+	
+	for i in range(length):
+		# Can only place a 1 if we've had at least 20 zeros since the last 1
+		if zeros_since_last_one >= 20:
+			# Randomly decide to place a 1 (30% chance)
+			if randf() > 0.7:
+				pattern.append(1)
+				zeros_since_last_one = 0  # Reset counter
+			else:
+				pattern.append(0)
+				zeros_since_last_one += 1
+		else:
+			# Must place a 0
+			pattern.append(0)
+			zeros_since_last_one += 1
+	print(pattern)
+	return pattern
+
+var pattern = generate_pattern(100)
 
 func make_road():
 	var road_scene = preload("res://scenes/road.tscn")
@@ -13,20 +38,33 @@ func make_road():
 	road.position.y = -230
 	road_list.append(road)
 	
-	add_child(road)
+	if roads_made < pattern.size() and pattern[roads_made] == 1:
+		road.get_node("sprite").play("intersection")
+	else:
+		road.get_node("sprite").play("default")
 	
+	
+	roads_made += 1
+	
+	add_child(road)
+
+
+
 func _ready() -> void:
+	
+	
+	
 	make_road()
 	
 func _process(delta: float) -> void:
 	
 	# Spawn new road when the last one reaches the threshold
 	if road_list[-1].position.y >= 800:
-		print("Spawn road")
+		
 		make_road()
 	
 	while road_list.size() > 0 and road_list[0].position.y >= 4000:
-		print("Despawn road at y =", road_list[0].position.y)
+		
 		var old_road = road_list.pop_front()
 		old_road.queue_free()
 	
